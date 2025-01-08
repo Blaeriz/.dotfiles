886557fb24fc04053dac82948a58dd791e33b491
Blaeriz <45209581+Blaeriz@users.noreply.github.com>
2025-01-09 04:16:47 +0530

    Add files via upload

---
+++ nvim/lua/options.lua
@@ -0,0 +1,8 @@
+require "nvchad.options"
+
+-- add yours here!
+vim.o.hlsearch = true
+vim.g.did_load_netrw = 1
+vim.o.relativenumber = true
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
